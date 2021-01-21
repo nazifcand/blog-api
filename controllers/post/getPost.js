6ca6dc740e2809e4f45a24e8e6c9352ea9f5b2c0
Nazif Can DURGUT <nazifcandurgutt@gmail.com>
2021-01-21 19:13:12 +0300

    get a post done

--- controllers/post/getPost.js
+++ controllers/post/getPost.js
@@ -1,3 +1,20 @@
-export default (req, res) => {
-  return res.send({ statusCode: 200, message: 'Get a post', slug: req.params.slug });
+import Post from '../../models/Post.js';
+
+export default async (req, res, next) => {
+
+  /* Find post */
+  const post =
+    await Post
+      .findOne({
+        slug: req.params.slug,
+        status: true,
+        type: 'post'
+      })
+      .then(result => result)
+      .catch(err => next({ statusCode: 500, message: 'OK', errors: err }));
+
+  /* Not found post */
+  if (!post) return next({ statusCode: 404, message: 'Böyle bir içerik bulunamadı!' });
+
+  return res.send({ statusCode: 200, message: 'OK', post });
 }
